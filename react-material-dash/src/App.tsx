4db18fc0c9b16da941f6ebe7f36ed81c016cdbd4
bk <btko@tuta.io>
2024-03-14 10:33:14 +0100

    some components added

--- react-material-dash/src/App.tsx
+++ react-material-dash/src/App.tsx
@@ -1,67 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.scss'
-import Button from '@mui/material/Button';
-import { DataGrid, GridColDef, GridRowsProp,  } from '@mui/x-data-grid';
-import { BarChart } from '@mui/x-charts/BarChart';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import PersistentDrawerLeft from './components/AppBarWithDrawer';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const rows: GridRowsProp = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-  ];
-  
-  const columns: GridColDef[] = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
-  ];
+  useEffect(() => {
+    document.documentElement.classList.add('html');
+    document.documentElement.classList.add('light-theme');
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <DataGrid rows={rows} columns={columns} />
-      <BarChart
-          series={[
-            { data: [35, 44, 24, 34] },
-            { data: [51, 6, 49, 30] },
-            { data: [15, 25, 30, 50] },
-            { data: [60, 50, 15, 25] },
-          ]}
-          height={290}
-          xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
-          margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-        />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        <br></br>
-        <Button variant="contained">Hello world</Button>
-      
-      </p>
+      <PersistentDrawerLeft></PersistentDrawerLeft>
     </>
   )
 }
