278b1bd2ea3a2e9ea9542a9aa5e8e11a54ced948
bk <btko@tuta.io>
2024-03-15 11:49:54 +0100

    main page styling

--- svelte-dash/src/components/side-drawer.svelte
+++ svelte-dash/src/components/side-drawer.svelte
@@ -2,18 +2,13 @@
 
 </script>
 
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2780; option 1</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2781; option 2</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2782; option 3</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2783; option 4</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2784; option 5</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2785; option 6</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2786; option 7</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2787; option 8</button>
-<button class="button-text-only" on:click={() => { alert("x")}}>&#x2788; option 9</button>
+<button class="button-text-only" on:click={() => { alert("x")}}>&#x2661; option 1</button>
+<button class="button-text-only" on:click={() => { alert("x")}}>&#x2662; option 2</button>
+<button class="button-text-only" on:click={() => { alert("x")}}>&#x2664; option 3</button>
+<button class="button-text-only" on:click={() => { alert("x")}}>&#x2667; option 4</button>
 
 <style lang="scss">
     @import '../styles/app.scss';
 
-    button { display: block; height: 40px; }
+    button { display: block; height: 60px; }
 </style>
--- svelte-dash/src/routes/+page.svelte
+++ svelte-dash/src/routes/+page.svelte
@@ -1,61 +1,101 @@
-<script>
-	import SideDrawer from '../components/side-drawer.svelte';
-
-    let showSideDrawer = true;
+<script lang="ts">
+    let expandSidebar = false;
+    const sidebarButtons = [ 
+        { icon: '&#x2661;', text: 'option 1', clickAction: () => { alert("1")}},
+        { icon: '&#x2662;', text: 'option 2', clickAction: () => { alert("2")}},
+        { icon: '&#x2664;', text: 'option 3', clickAction: () => { alert("3")}},
+        { icon: '&#x2667;', text: 'option 4', clickAction: () => { alert("4")}},
+    ]
 </script>
 
-<div class={'main-page' + (showSideDrawer ? ' with-side' : '')}>
+<div class={'main-page' + (expandSidebar ? ' with-sidebar-expanded' : '')}>
     <div class="header">
-        <button on:click={() => { showSideDrawer = !showSideDrawer}} class="button-text-only side-menu-button">
-            &#x2630;
+        <button on:click={() => { expandSidebar = !expandSidebar}} class="button-text-only header-menu-button">
+            {#if expandSidebar} &#x276E; {:else} &#x2630; {/if} 
         </button>
-        <button on:click={() => { }} class="button-text-only nav-button">
+        <button on:click={() => {  }} class="button-text-only nav-button">
             Page 1
         </button>
     </div>
-    {#if showSideDrawer}
-    <div class="side">
-        <SideDrawer></SideDrawer>
-    </div>
-    {/if}
-    <div class="content">
+    <div class="content"></div>
+    <div class="sidebar">
+        {#each sidebarButtons as button}
+        <button class="button-text-only side-button" on:click={button.clickAction}>
+            <span class="image">{@html button.icon}</span><span class="text">{button.text}</span>
+        </button>
+        {/each}
     </div>
 </div>
 
 <style lang="scss">
     @import "../styles/app.scss";
 
+    //reset all
+    :global(body) { margin: 0; padding: 0; font-family: "Source Code Pro", monospace; font-optical-sizing: auto; font-weight: 500; font-style: normal; }
+	:global(*) { box-sizing: border-box; background-color: transparent;	margin: 0; padding: 0; width: 100%; 
+                 font-family: inherit; font-optical-sizing: inherit; font-weight: inherit; font-style: inherit; }
+
+    //set global styles
+    :global(body) { margin: 0; display: flex; place-items: center; min-width: 320px; min-height: 100vh; background-color: $background-color; }
+    
+
+    // main page styles
     $header-size: 60px;
     $side-size: 200px;
 
     .main-page {
-        display: grid;
-        grid-template-columns: 0 1fr;
-        grid-template-rows: $header-size 1fr;
-        grid-template-areas: 
-            "side header"
-            "side content";
-        -webkit-transition: all 1s linear;
-        -moz-transition: all 1s linear;
-        -o-transition: all 1s linear;
-        transition: all 1s linear;
+        * {
+            -webkit-transition: all 0.1s ease-in-out;
+            -moz-transition: all 0.1s ease-in-out;
+            -o-transition: all 0.1s ease-in-out;
+            transition: all 0.1s ease-in-out;
+        }
     }
 
-    .with-side {
-        grid-template-columns: $side-size 1fr;
-        -webkit-transition: all 1s linear;
-        -moz-transition: all 1s linear;
-        -o-transition: all 1s linear;
-        transition: all 1s linear;
-    }
-
-    button { line-height: $header-size; width: auto; padding-left: 1rem; padding-right: 1rem; }
+    button { line-height: $header-size; }
     span { line-height: $header-size;}
 
-    .header { grid-area: header; background-color: lightblue; }
-    .side { grid-area: side; padding-top: $header-size; background-color: gray;}
-    .content { grid-area: content; }
+    .header { 
+        position: fixed; top: 0; left: 0;
+        z-index: 3; 
+        background-color: $primary-color; 
+        color: $text-color; 
+        width: 100vw;
+    }
+    .content { 
+        position: fixed; top: 0; left: $header-size;
+        z-index: 2; 
+        padding: calc(10px + $header-size) 10px 10px 10px;
+        background-color: $background-color;
+        height: 100vh;
+        width: calc(100vw - $header-size);
+        overflow-y: auto;
+    }
+    .sidebar { 
+        position: fixed; top: 0; left: 0;
+        z-index: 1;
+        padding-top: $header-size; 
+        border-right: 1px solid $primary-color;
+        button {
+            background-color: $primary-color-dark;
+        }
+        width: $side-size;
+        height: 100vh;
+        background-color: $primary-color-dark;
+    }
 
-    .side-menu-button { width: $header-size; }
+    .with-sidebar-expanded{
+        .header { left: $side-size; width: calc(100vw - $side-size); }
+        .content { left: $side-size; width: calc(100vw - $side-size); }
+    }
+
+    .header-menu-button { width: $header-size; background-color: $primary-color; }
+    .nav-button { width: auto; padding-left: 1rem; padding-right: 1rem; background-color: $primary-color; }
+    .side-button { 
+        display: grid; grid-template-columns: 60px 1fr; grid-template-rows: 1fr;
+        
+        .image { grid-column: 1; }
+        .text { grid-column: 2; text-align: left; padding-left: 10px; }
+    }
 </style>
 
