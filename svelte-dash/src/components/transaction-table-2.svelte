be683630478665adbe7c30a64da176b5efd03518
bk <btko@tuta.io>
2024-05-03 17:59:04 +0200

    adding filters

--- svelte-dash/src/components/transaction-table-2.svelte
+++ svelte-dash/src/components/transaction-table-2.svelte
@@ -3,6 +3,7 @@
 	import { accountStoreReadOnly, categoryStoreReadOnly, accountCurrencyMap } from '../services/store';
 	import { onMount } from 'svelte';
 	import { TransactionService } from '../services/transaction-service';
+	import { type TransactionSearchFilters } from '../models/transaction-search-filters';
 
     const headers = ['data', 'konto', 'kategoria', 'kwota', '-', 'komentarz'] 
     const accounts = $accountStoreReadOnly 
@@ -15,6 +16,12 @@
     let page = 0
     let pageCount = 0
     $: emptyRowsCount = pageSize - displayTransactions.length > 0 ? pageSize - displayTransactions.length : 0
+    // filters: 
+    let dateFilterText = ''
+    let accountFilterText = ''
+    let categoryFilterText = ''
+    let amountFilterText = ''
+    let commentFilterText = ''
 
     let filtersVisible = false
     let selectedRow = -1
@@ -25,6 +32,46 @@
         loadTransactionPage(0)
     })
 
+    const getFilters = (): TransactionSearchFilters => {
+        const getDate = (dateString: string) => {
+            return new Date((new Date()).setTime(new Date(dateString).getTime()+12*60*60*1000))
+        } 
+
+        const searchFilters: TransactionSearchFilters = {ownerId: 1}
+
+        if(dateFilterText.length){
+            const dateStrings = dateFilterText.split(',')
+            console.log(dateStrings.length)
+            console.log(Date.parse(dateStrings[0]))
+            console.log(dateStrings[1])
+            //this is all wrong - welcome fucking timezones and time formats!
+            if (dateStrings.length > 0 && !isNaN(Date.parse(dateStrings[0]))) { searchFilters.dateFrom = getDate(dateStrings[0])}
+            if (dateStrings.length > 1 && !isNaN(Date.parse(dateStrings[1]))) { searchFilters.dateTo = getDate(dateStrings[1]) }
+        }
+
+        if (accountFilterText.length){
+            const accountIds = accountFilterText.split(',').filter(x => x.length && !isNaN(+x)).map(x => parseInt(x) )
+            if (accountIds.length) { searchFilters.accounts = accountIds }
+        }
+
+        if (categoryFilterText.length){
+            const categoryIds = categoryFilterText.split(',').filter(x => x.length && !isNaN(+x)).map(x => parseInt(x) )
+            if (categoryIds.length) { searchFilters.categories = categoryIds }
+        }
+
+        if (amountFilterText.length){
+            const amountStrings = amountFilterText.split(',')
+            if (amountStrings.length > 0 && !isNaN(+amountStrings[0])) { searchFilters.amountFrom = +amountStrings[0] }
+            if (amountStrings.length > 1 && !isNaN(+amountStrings[1])) { searchFilters.amountTo = +amountStrings[1] }
+        }
+
+        if (commentFilterText.length){ searchFilters.comment = commentFilterText }
+
+        console.log(searchFilters)
+
+        return searchFilters
+    }
+
     const loadTransactionPage = async (pageChange: number) => {
         let nextPage = page + pageChange
         if(nextPage < 0) return
@@ -32,12 +79,12 @@
         dataLoaded = false
         await sleep(500)       
         try{
-            const transactionCountResult = await TransactionService.SearchTransactionsCount({ownerId: 1})
+            const transactionCountResult = await TransactionService.SearchTransactionsCount(getFilters())
             pageCount = Math.max(Math.ceil(transactionCountResult / pageSize), 1)
 
             if(pageChange === 0) nextPage = pageCount -1 // initial load -> go to last page
 
-            const transactionSearchResult = await TransactionService.SearchTransactions({ownerId: 1, take: pageSize, offset: nextPage*pageSize})
+            const transactionSearchResult = await TransactionService.SearchTransactions({...getFilters(), take: pageSize, offset: nextPage*pageSize})
 
             if(transactionSearchResult.length > 0){
                 page = nextPage
@@ -63,6 +110,7 @@
     }
 
     const filtersClick = () => { filtersVisible = !filtersVisible }
+    const filtersConfirmed = () => { filtersVisible = false; loadTransactionPage(0) }
 </script>
 
 <div>
@@ -98,17 +146,20 @@
     </table>
     <table class={filtersVisible ? '' : 'hidden'}>
         <tr>
-            <td class="aln-l w-20pc"><input type="text" placeholder="data: od, do"/></td>
-            <td class="aln-l w-20pc"><input type="text" placeholder="konto: id, id"/></td>
-            <td class="aln-l w-20pc"><input type="text" placeholder="kategoria: id, id"/></td>
-            <td class="aln-l w-20pc"><input type="text" placeholder="kwota: od, do"/></td>
-            <td class="aln-l w-20pc"><input type="text" placeholder="komentarz"/></td>
+            <td class="aln-l w-20pc"><input type="text" bind:value={dateFilterText} placeholder="data: od, do"/></td>
+            <td class="aln-l w-20pc"><input type="text" bind:value={accountFilterText} placeholder="konto: id, id"/></td>
+            <td class="aln-l w-20pc"><input type="text" bind:value={categoryFilterText} placeholder="kategoria: id, id"/></td>
+            <td class="aln-l w-20pc"><input type="text" bind:value={amountFilterText} placeholder="kwota: od, do"/></td>
+            <td class="aln-l w-20pc"><input type="text" bind:value={commentFilterText} placeholder="komentarz"/></td>
         </tr>
     </table>
     <div class="actions">
-        <button class="button-outlined" on:click={() => { filtersClick() }}>{@html filtersVisible ? '&#x2713;' : '&#x2637;'}</button>
+        {#if filtersVisible}
+        <button class="button-outlined" on:click={() => { filtersConfirmed() }}>&#x2699;</button>
+        {/if}
+        <button class="button-outlined" on:click={() => { filtersClick() }}>&#x2637;</button>
         <button class="button-outlined" disabled={page === 0} on:click={() => { loadTransactionPage(-1) }}>&#x276E;</button>
-        <span>{`${page + 1} / ${pageCount}`}</span>
+        <div>{`${page + 1} / ${pageCount}`}</div>
         <button class="button-outlined" disabled={page + 1 === pageCount} on:click={() => { loadTransactionPage(1) }}>&#x276F;</button>
     </div>
 </div>
@@ -116,7 +167,7 @@
 <style lang="scss">
     @import '../styles/app.scss';
     
-    .actions { margin: 10px; text-align: right; }
+    .actions { margin: 10px; display: flex; align-items: center; gap: 10px; justify-content: end;}
     .selected { background-color: $accent-color-light;}
     input { width: 100%;}
     .hidden { visibility: hidden;}
